6fbab37f915ea33924cc462ffd6288864fd77c82
Grzegorz Hardejewicz <ghardejewicz@gmail.com>
2022-01-12 09:24:53 +0100

    Added JS animation to nav_item

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const nav = document.querySelector('.nav');
+const brgBtn = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.nav__item');
+
+const handleNav = () => {
+	nav.classList.toggle('nav--active');
+	allNavItems.forEach((navItem) => {
+		navItem.addEventListener('click', () => {
+			nav.classList.remove('nav--active');
+		});
+	});
+
+	handleNavItemsAnimation();
+};
+
+brgBtn.addEventListener('click', handleNav);
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0;
+
+	allNavItems.forEach((navItem) => {
+		navItem.classList.toggle('nav-item-animation');
+		navItem.style.animationDelay = '.' + delayTime + 's';
+		delayTime++;
+	});
+};
+
+// const navItems = document.querySelector('.nav__items');
+// const removeNav = () => {
+// 	nav.classList.remove('nav--active');
+// };
+
+// navItems.addEventListener('click', removeNav);
